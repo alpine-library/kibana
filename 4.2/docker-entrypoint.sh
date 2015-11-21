d4b8d62c1e291dcd0be260fee3d27b4d41688068
Administrator <m@3ko.fr>
2015-11-21 21:32:05 +0100

    fix:entrypoint.sh

--- 4.2/docker-entrypoint.sh
+++ 4.2/docker-entrypoint.sh
@@ -3,9 +3,4 @@
 export ELASTICSEARCH_URL=${ELASTICSEARCH_URL:-"http://localhost:9200"}
 echo ELASTICSEARCH_URL=${ELASTICSEARCH_URL}
 
-# Run as user "kibana" if the command is "kibana"
-if [ "$1" = 'kibana' ]; then
-	set -- gosu kibana /opt/kibana/bin/kibana -e ${ELASTICSEARCH_URL}
-fi
-
-exec "$@"
+gosu kibana /opt/kibana/bin/kibana -e ${ELASTICSEARCH_URL}
