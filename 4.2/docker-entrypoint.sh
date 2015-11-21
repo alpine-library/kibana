2a091895fe4bfd465c53440f1c7ff2813fd617d4
Administrator <m@3ko.fr>
2015-11-21 21:51:28 +0100

    fix:entrypoint.sh perms

--- 4.2/docker-entrypoint.sh
+++ 4.2/docker-entrypoint.sh
@@ -3,4 +3,4 @@
 export ELASTICSEARCH_URL=${ELASTICSEARCH_URL:-"http://localhost:9200"}
 echo ELASTICSEARCH_URL=${ELASTICSEARCH_URL}
 
-gosu kibana /opt/kibana/bin/kibana -e ${ELASTICSEARCH_URL}
+/opt/kibana/bin/kibana -e ${ELASTICSEARCH_URL}
